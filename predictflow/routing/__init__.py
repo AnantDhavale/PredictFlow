e33b09214661a7dacfed63198d7dcf0d9f64c9cd
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2025-11-24 15:24:19 -0600

    Update __init__.py

--- predictflow/routing/__init__.py
+++ predictflow/routing/__init__.py
@@ -1,1 +1,20 @@
+"""
+PredictFlow Intelligent Routing Module
+--------------------------------------
 
+Provides context-aware resource assignment and routing strategies.
+"""
+
+from predictflow.routing.resource_router import (
+    ResourceRouter,
+    ResourceProfile,
+    RoutingStrategy,
+    RoutingDecision
+)
+
+__all__ = [
+    'ResourceRouter',
+    'ResourceProfile', 
+    'RoutingStrategy',
+    'RoutingDecision'
+]
