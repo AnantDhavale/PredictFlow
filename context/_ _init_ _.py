d5809557120682ce2f6054c9e4c4f0b079388189
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2025-11-24 15:28:55 -0600

    Create _ _init_ _.py

---
+++ context/_ _init_ _.py
@@ -0,0 +1,10 @@
+"""
+PredictFlow Context Awareness Module
+------------------------------------
+
+Provides multi-dimensional context collection and analysis.
+"""
+
+from predictflow.context.context_engine import ContextEngine, ContextSnapshot
+
+__all__ = ['ContextEngine', 'ContextSnapshot']
