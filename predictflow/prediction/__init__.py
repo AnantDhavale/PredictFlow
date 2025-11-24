56a6b0d8862c3a812e415cacada791b6644037a4
AnantDhavale <47012267+AnantDhavale@users.noreply.github.com>
2025-11-24 15:36:25 -0600

    Update __init__.py

--- predictflow/prediction/__init__.py
+++ predictflow/prediction/__init__.py
@@ -1,1 +1,19 @@
 
+"""
+PredictFlow LSTM Failure Prediction Module
+------------------------------------------
+
+Provides time-series based failure prediction for workflow steps.
+"""
+
+from predictflow.prediction.lstm_predictor import LSTMFailurePredictor
+from predictflow.prediction.data_processor import FailureDataProcessor
+from predictflow.prediction.trainer import FailurePredictionTrainer
+from predictflow.prediction.inference import FailurePredictionEngine
+
+__all__ = [
+    'LSTMFailurePredictor',
+    'FailureDataProcessor',
+    'FailurePredictionTrainer',
+    'FailurePredictionEngine'
+]
